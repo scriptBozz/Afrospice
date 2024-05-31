b4a34729b8cefe3fc547997fd15956c5ade16ad6
uche <uceenathans@gmail.com>
2024-05-31 17:05:13 +0200

    added Cors options

--- api/src/app.ts
+++ api/src/app.ts
@@ -19,6 +19,8 @@ passport.use(jwtstrategy)
 const corsOptions = {
   origin: "https://afrospicefe.onrender.com", // Specify the origin you want to allow
   optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
 };
 
 // Use the CORS middleware with the options
